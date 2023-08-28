1ecf4adaa87273a0a1b5f0c119441c429c946829
Rehan <rehan101err0r@gmail.com>
2023-08-28 16:46:07 +0530

    admin

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    // ...add more providers here
+  ],
+
+  adapter: MongoDBAdapter(clientPromise)
+}
+
+export default NextAuth(authOptions)
